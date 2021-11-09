e305c30c8ef20f12d8846c009ffc695c7e6b2418
Nicolas Chamo <nicolas@chamo.com.ar>
2021-11-09 13:24:52 -0300

    feat: add swap with 0x

--- contracts/DCAHubCompanion/DCAHubCompanionSwapHandler.sol
+++ contracts/DCAHubCompanion/DCAHubCompanionSwapHandler.sol
@@ -9,7 +9,8 @@ import './DCAHubCompanionParameters.sol';
 abstract contract DCAHubCompanionSwapHandler is DeadlineValidation, DCAHubCompanionParameters, IDCAHubCompanionSwapHandler {
   enum SwapPlan {
     NONE,
-    SWAP_FOR_CALLER
+    SWAP_FOR_CALLER,
+    SWAP_WITH_0X
   }
 
   struct SwapData {
@@ -18,6 +19,15 @@ abstract contract DCAHubCompanionSwapHandler is DeadlineValidation, DCAHubCompan
   }
 
   using SafeERC20 for IERC20;
+
+  // solhint-disable-next-line var-name-mixedcase
+  address public immutable ZRX;
+
+  // solhint-disable-next-line var-name-mixedcase
+  constructor(address _ZRX) {
+    if (_ZRX == address(0)) revert IDCAHubCompanion.ZeroAddress();
+    ZRX = _ZRX;
+  }
 
   function swapForCaller(
     address[] calldata _tokens,
@@ -46,6 +56,48 @@ abstract contract DCAHubCompanionSwapHandler is DeadlineValidation, DCAHubCompan
     }
   }
 
+  function swapWith0x(
+    address[] calldata _tokens,
+    IDCAHub.PairIndexes[] calldata _pairsToSwap,
+    bytes[] calldata _callsTo0x,
+    address _leftoverRecipient,
+    uint256 _deadline
+  ) external checkDeadline(_deadline) returns (IDCAHub.SwapInfo memory) {
+    return _swapWith0x(_tokens, _pairsToSwap, _callsTo0x, _leftoverRecipient, false, _deadline);
+  }
+
+  function swapWith0xAndShareLeftoverWithHub(
+    address[] calldata _tokens,
+    IDCAHub.PairIndexes[] calldata _pairsToSwap,
+    bytes[] calldata _callsTo0x,
+    address _leftoverRecipient,
+    uint256 _deadline
+  ) external checkDeadline(_deadline) returns (IDCAHub.SwapInfo memory) {
+    return _swapWith0x(_tokens, _pairsToSwap, _callsTo0x, _leftoverRecipient, true, _deadline);
+  }
+
+  function _swapWith0x(
+    address[] calldata _tokens,
+    IDCAHub.PairIndexes[] calldata _pairsToSwap,
+    bytes[] calldata _callsTo0x,
+    address _leftoverRecipient,
+    bool _sendToProvideLeftoverToHub,
+    uint256 _deadline
+  ) internal checkDeadline(_deadline) returns (IDCAHub.SwapInfo memory _swapInfo) {
+    uint256[] memory _borrow = new uint256[](_tokens.length);
+    bytes memory _swapData = abi.encode(
+      CallbackData0x({leftoverRecipient: _leftoverRecipient, callsTo0x: _callsTo0x, sendToProvideLeftoverToHub: _sendToProvideLeftoverToHub})
+    );
+    _swapInfo = hub.swap(
+      _tokens,
+      _pairsToSwap,
+      address(this),
+      address(this),
+      _borrow,
+      abi.encode(SwapData({plan: SwapPlan.SWAP_WITH_0X, data: _swapData}))
+    );
+  }
+
   // solhint-disable-next-line func-name-mixedcase
   function DCAHubSwapCall(
     address _sender,
@@ -59,8 +111,62 @@ abstract contract DCAHubCompanionSwapHandler is DeadlineValidation, DCAHubCompan
     SwapData memory _swapData = abi.decode(_data, (SwapData));
     if (_swapData.plan == SwapPlan.SWAP_FOR_CALLER) {
       _handleSwapForCallerCallback(_tokens, _swapData.data);
+    } else if (_swapData.plan == SwapPlan.SWAP_WITH_0X) {
+      _handleSwapWith0xCallback(_tokens, _swapData.data);
     } else {
       revert UnexpectedSwapPlan();
+    }
+  }
+
+  struct CallbackData0x {
+    address leftoverRecipient;
+    bytes[] callsTo0x;
+    // This flag is just a way to make transactions cheapers. If Mean Finance is executing the swap, then it's the same for us
+    // if the leftover tokens go to the hub, or to another address. But, it's cheaper in terms of gas to send them to the hub
+    bool sendToProvideLeftoverToHub;
+  }
+
+  function _handleSwapWith0xCallback(IDCAHub.TokenInSwap[] calldata _tokens, bytes memory _data) internal {
+    CallbackData0x memory _callbackData = abi.decode(_data, (CallbackData0x));
+    address _hub = address(hub);
+    address _zrx = ZRX;
+
+    // Approve ZRX
+    for (uint256 i; i < _tokens.length; i++) {
+      IDCAHub.TokenInSwap memory _tokenInSwap = _tokens[i];
+      if (_tokenInSwap.toProvide > 0) {
+        IERC20(_tokenInSwap.token).approve(_zrx, _tokenInSwap.toProvide);
+      }
+    }
+
+    // Execute swaps
+    for (uint256 i; i < _callbackData.callsTo0x.length; i++) {
+      // solhint-disable-next-line avoid-low-level-calls
+      (bool success, ) = _zrx.call{value: 0}(_callbackData.callsTo0x[i]);
+      require(success, 'Swapper: ZRX trade reverted');
+    }
+
+    // Send remaining tokens to either hub, or leftover recipient
+    for (uint256 i; i < _tokens.length; i++) {
+      IERC20 _erc20 = IERC20(_tokens[i].token);
+      uint256 _toProvide = _tokens[i].toProvide;
+      uint256 _balance = _erc20.balanceOf(address(this));
+      if (_balance > 0) {
+        if (_toProvide > 0) {
+          // If the hub expects some tokens in return, check if we want to send the whole balance or just the necessary amount
+          if (_callbackData.sendToProvideLeftoverToHub || _balance == _toProvide) {
+            // Send everything
+            _erc20.safeTransfer(_hub, _balance);
+          } else {
+            // Send necessary to hub, and the rest to the leftover recipient
+            _erc20.safeTransfer(_hub, _toProvide);
+            _erc20.safeTransfer(_callbackData.leftoverRecipient, _balance - _toProvide);
+          }
+        } else {
+          // Since the hub doesn't expect any amount of this token, send everything to the left over recipient
+          _erc20.safeTransfer(_callbackData.leftoverRecipient, _balance);
+        }
+      }
     }
   }
 
