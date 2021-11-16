6e3be3d401b9032c7272fc5ddd56b92306c897d7
Nicolas Chamo <nicolas@chamo.com.ar>
2021-11-16 09:43:35 -0300

    test: fix integration test

--- test/integration/DCAHubCompanion/dca-hub-companion-swap-handler.spec.ts
+++ test/integration/DCAHubCompanion/dca-hub-companion-swap-handler.spec.ts
@@ -2,24 +2,23 @@ import { expect } from 'chai';
 import { deployments, ethers, getNamedAccounts } from 'hardhat';
 import { JsonRpcSigner, TransactionResponse } from '@ethersproject/providers';
 import { constants, wallet } from '@test-utils';
-import { contract, given, then, when } from '@test-utils/bdd';
+import { given, then, when } from '@test-utils/bdd';
 import evm, { snapshot } from '@test-utils/evm';
 import { DCAHubCompanion, IERC20 } from '@typechained';
-import { DCAHub, IPriceOracle } from '@mean-finance/dca-v2-core/typechained';
+import { DCAHub } from '@mean-finance/dca-v2-core/typechained';
 import { abi as DCA_HUB_ABI } from '@mean-finance/dca-v2-core/artifacts/contracts/DCAHub/DCAHub.sol/DCAHub.json';
-import { abi as IPRICE_ORACLE_ABI } from '@mean-finance/dca-v2-core/artifacts/contracts/interfaces/oracles/IPriceOracle.sol/IPriceOracle.json';
 import { getNodeUrl } from '@utils/network';
 import { abi as IERC20_ABI } from '@openzeppelin/contracts/build/contracts/IERC20.json';
 import { BigNumber, utils } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signers';
 import { SwapInterval } from '@test-utils/interval-utils';
-import zrx, { QuoteResponse } from '@test-utils/zrx';
+import zrx from '@test-utils/zrx';
 
 const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 const USDC_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
 const WETH_WHALE_ADDRESS = '0xf04a5cc80b1e94c69b48f5ee68a08cd2f09a7c3e';
 
-contract('DCAHubCompanionSwapHandler', () => {
+describe('Single pair swap with DEX', () => {
   let WETH: IERC20;
   let USDC: IERC20;
   let governor: JsonRpcSigner;
@@ -27,7 +26,7 @@ contract('DCAHubCompanionSwapHandler', () => {
   let cindy: SignerWithAddress, recipient: SignerWithAddress;
   let DCAHubCompanion: DCAHubCompanion;
   let DCAHub: DCAHub;
-  let oracle: IPriceOracle;
+  let initialPerformedSwaps: number;
   let snapshotId: string;
 
   const RATE = utils.parseEther('0.1');
@@ -42,7 +41,6 @@ contract('DCAHubCompanionSwapHandler', () => {
     await deployments.fixture('DCAHubCompanion', { keepExistingDeployments: false });
     DCAHub = await ethers.getContract('DCAHub');
     DCAHubCompanion = await ethers.getContract('DCAHubCompanion');
-    oracle = await ethers.getContractAt(IPRICE_ORACLE_ABI, await DCAHub.oracle());
 
     const namedAccounts = await getNamedAccounts();
     const governorAddress = namedAccounts.governor;
@@ -70,30 +68,74 @@ contract('DCAHubCompanionSwapHandler', () => {
       cindy.address,
       []
     );
+    initialPerformedSwaps = await performedSwaps();
 
     snapshotId = await snapshot.take();
   });
   beforeEach('Deploy and configure', async () => {
     await snapshot.revert(snapshotId);
   });
-  describe('swapWith0x', () => {
-    when('executing a swap with 0x', () => {
-      let zrxQuote: QuoteResponse;
-      let initialPerformedSwaps: number;
-      let reward: BigNumber, toProvide: BigNumber, sentToAgg: BigNumber, receivedFromAgg: BigNumber;
 
-      given(async () => {
-        initialPerformedSwaps = await performedSwaps();
-        const tokensInSwap = [USDC_ADDRESS, WETH_ADDRESS];
-        const indexesInSwap = [{ indexTokenA: 0, indexTokenB: 1 }];
-        zrxQuote = await zrx.quote({
+  describe('swap with dex', () => {
+    swapWithDexTest({
+      dex: '0x',
+      isSwapAndTransfer: false,
+      sendLeftoverToHub: false,
+      quote: () =>
+        zrx.quote({
           chainId: 1,
           sellToken: WETH_ADDRESS,
           buyToken: USDC_ADDRESS,
           sellAmount: RATE.sub(100), // We sell a little less than necessary, so that we can test the leftover
           sippagePercentage: 0.001,
-        });
-        const swapTx = await DCAHubCompanion.swapWith0x(tokensInSwap, indexesInSwap, [zrxQuote.data], recipient.address, constants.MAX_UINT_256);
+        }),
+    });
+    swapWithDexTest({
+      dex: '0x',
+      isSwapAndTransfer: false,
+      sendLeftoverToHub: true,
+      quote: () =>
+        zrx.quote({
+          chainId: 1,
+          sellToken: WETH_ADDRESS,
+          buyToken: USDC_ADDRESS,
+          sellAmount: RATE.sub(100), // We sell a little less than necessary, so that we can test the leftover
+          sippagePercentage: 0.001,
+        }),
+    });
+  });
+
+  function swapWithDexTest({
+    dex,
+    isSwapAndTransfer,
+    sendLeftoverToHub,
+    quote,
+  }: {
+    quote: () => Promise<{ to: string; data: string }>;
+    dex: string;
+    isSwapAndTransfer: boolean;
+    sendLeftoverToHub: boolean;
+  }) {
+    const title = `executing a swap with ${dex}, ${isSwapAndTransfer ? 'with' : 'without'} swap and transfer and ${
+      !sendLeftoverToHub ? 'without ' : ''
+    }sending leftover to hub`;
+    when(title, () => {
+      let reward: BigNumber, toProvide: BigNumber, sentToAgg: BigNumber, receivedFromAgg: BigNumber;
+      given(async () => {
+        const tokensInSwap = [USDC_ADDRESS, WETH_ADDRESS];
+        const indexesInSwap = [{ indexTokenA: 0, indexTokenB: 1 }];
+        const dexQuote = await quote();
+        await DCAHubCompanion.connect(governor).defineDexSupport(dexQuote.to, true);
+        const dexFunction = sendLeftoverToHub ? 'swapWithDexAndShareLeftoverWithHub' : 'swapWithDex';
+        const swapTx = await DCAHubCompanion[dexFunction](
+          dexQuote.to,
+          tokensInSwap,
+          indexesInSwap,
+          [dexQuote.data],
+          isSwapAndTransfer,
+          recipient.address,
+          constants.MAX_UINT_256
+        );
         ({ reward, toProvide, receivedFromAgg, sentToAgg } = await getTransfers(swapTx));
       });
       then('swap is executed', async () => {
@@ -103,16 +145,29 @@ contract('DCAHubCompanionSwapHandler', () => {
         const hubWETHBalance = await WETH.balanceOf(DCAHub.address);
         const hubUSDCBalance = await USDC.balanceOf(DCAHub.address);
         expect(hubWETHBalance).to.equal(RATE.mul(AMOUNT_OF_SWAPS - 1));
-        expect(hubUSDCBalance).to.equal(toProvide);
+        if (!sendLeftoverToHub) {
+          expect(hubUSDCBalance).to.equal(toProvide);
+        } else {
+          expect(hubUSDCBalance).to.equal(receivedFromAgg);
+        }
       });
-      then('all surpluss is sent to leftover recipient', async () => {
-        const recipientUSDCBalance = await USDC.balanceOf(recipient.address);
+      then('all reward surpluss is sent to leftover recipient', async () => {
         const recipientWETHBalance = await WETH.balanceOf(recipient.address);
-        expect(recipientUSDCBalance).to.equal(receivedFromAgg.sub(toProvide));
         expect(recipientWETHBalance).to.equal(reward.sub(sentToAgg));
       });
+      if (!sendLeftoverToHub) {
+        then('all "toProvide" surpluss is sent to leftover recipient', async () => {
+          const recipientUSDCBalance = await USDC.balanceOf(recipient.address);
+          expect(recipientUSDCBalance).to.equal(receivedFromAgg.sub(toProvide));
+        });
+      } else {
+        then('leftover recipient has no "toProvide" balance', async () => {
+          const recipientUSDCBalance = await USDC.balanceOf(recipient.address);
+          expect(recipientUSDCBalance).to.equal(0);
+        });
+      }
     });
-  });
+  }
 
   async function performedSwaps(): Promise<number> {
     const { performedSwaps } = await DCAHub.swapData(USDC_ADDRESS, WETH_ADDRESS, SwapInterval.ONE_MINUTE.mask);
