0dbeb9771ad326d4810019fcb5ea5a3c7078fff6
Nicolas Chamo <nicolas@chamo.com.ar>
2021-11-19 16:38:20 -0300

    feat: add work to keep3r job (#25, #26)

--- contracts/interfaces/IDCAKeep3rJob.sol
+++ contracts/interfaces/IDCAKeep3rJob.sol
@@ -2,27 +2,64 @@
 pragma solidity >=0.8.7 <0.9.0;
 
 import './IDCAHubCompanion.sol';
+import './IKeep3rJobs.sol';
 import './utils/IGovernable.sol';
 
 interface IDCAKeep3rJob is IGovernable {
+  /// @notice A call to execute work
+  struct WorkCall {
+    // The actual call to the companion contract
+    bytes companionCall;
+    uint256 nonce;
+    uint256 chainId;
+    uint256 deadline;
+  }
+
   /// @notice Thrown when one of the parameters is a zero address
   error ZeroAddress();
+
+  /// @notice Thrown when a user tries to execute work but the signature is invalid
+  error SignerCannotSignWork();
+
+  /// @notice Thrown when a user tries to execute work with an invalid nonce
+  error InvalidNonce();
+
+  /// @notice Thrown when a user tries to execute work with an expired deadline
+  error DeadlineExpired();
+
+  /// @notice Thrown when a user tries to execute work with an invalid chain id
+  error InvalidChainId();
+
+  /// @notice Thrown when a user tries to execute work but the call to the companion fails
+  error CompanionCallFailed();
+
+  /// @notice Thrown when a non keep3r address tries to execute work
+  error NotAKeeper();
+
+  /// @notice Emitted when a new companion is set
+  /// @param newCompanion The new companion
+  event NewCompanionSet(IDCAHubCompanion newCompanion);
 
   /// @notice Emitted when signing permission is modified for an address
   /// @param affected The affected address
   /// @param canSign Whether the affected address can now sign work or not
   event ModifiedAddressPermission(address affected, bool canSign);
-  /// @notice Emitted when a new companion is set
-  /// @param newCompanion The new companion
-  event NewCompanionSet(IDCAHubCompanion newCompanion);
 
   /// @notice Returns the companion address
   /// @return The companion address
   function companion() external returns (IDCAHubCompanion);
 
+  /// @notice Returns the Keep3r address
+  /// @return The Keep3r address address
+  function keep3r() external returns (IKeep3rJobs);
+
   /// @notice Returns whether the given address can sign work or not
   /// @return If it can sign work or not
   function canAddressSignWork(address _address) external returns (bool);
+
+  /// @notice Returns the nonce to use when calling `work`
+  /// @return The nonce to use
+  function nonce() external returns (uint256);
 
   /// @notice Sets a new companion address
   /// @dev Will revert with ZeroAddress if the zero address is passed
@@ -34,4 +71,16 @@ interface IDCAKeep3rJob is IGovernable {
   /// @param _address The address to modify permissions
   /// @param _canSign Whether the given address will be able to sign work or not
   function setIfAddressCanSign(address _address, bool _canSign) external;
+
+  /// @notice Takes an encoded call to execute against the companion contract, and executes it
+  /// @dev Will revert with:
+  /// NotAKeeper if the caller is not a keep3r
+  /// SignerCannotSignWork if the address who signed the message cannot sign work
+  /// InvalidNonce if the nonce is invalid
+  /// DeadlineExpired if the deadline has expired
+  /// InvalidChainId if the chain id is invalid
+  /// CompanionCallFailed if the call to the companion failed
+  /// @param _bytes An encoded `WorkCall`
+  /// @param _signature A signature of `_bytes`
+  function work(bytes calldata _bytes, bytes calldata _signature) external;
 }
--- test/unit/DCAKeep3rJob/dca-keep3r-job.spec.ts
+++ test/unit/DCAKeep3rJob/dca-keep3r-job.spec.ts
@@ -1,30 +1,40 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { behaviours, constants, wallet } from '@test-utils';
 import { contract, given, then, when } from '@test-utils/bdd';
 import { snapshot } from '@test-utils/evm';
-import { DCAKeep3rJob, DCAKeep3rJob__factory, IDCAHubCompanion } from '@typechained';
+import { DCAKeep3rJobMock, DCAKeep3rJobMock__factory, IDCAHubCompanion, IKeep3rJobs } from '@typechained';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signers';
 import { FakeContract, smock } from '@defi-wonderland/smock';
 import { TransactionResponse } from '@ethersproject/abstract-provider';
+import { BigNumber, BigNumberish, BytesLike, utils, Wallet } from 'ethers';
+import moment from 'moment';
+
+chai.use(smock.matchers);
 
 contract('DCAKeep3rJob', () => {
-  let governor: SignerWithAddress, signer: SignerWithAddress;
-  let DCAKeep3rJob: DCAKeep3rJob;
-  let DCAKeep3rJobFactory: DCAKeep3rJob__factory;
+  let governor: SignerWithAddress, signer: SignerWithAddress, random: SignerWithAddress;
+  let DCAKeep3rJob: DCAKeep3rJobMock;
+  let DCAKeep3rJobFactory: DCAKeep3rJobMock__factory;
   let DCAHubCompanion: FakeContract<IDCAHubCompanion>;
+  let keep3r: FakeContract<IKeep3rJobs>;
+  let chainId: BigNumber;
   let snapshotId: string;
 
   before('Setup accounts and contracts', async () => {
-    [, governor, signer] = await ethers.getSigners();
+    [, governor, signer, random] = await ethers.getSigners();
     DCAHubCompanion = await smock.fake('IDCAHubCompanion');
-    DCAKeep3rJobFactory = await ethers.getContractFactory('contracts/DCAKeep3rJob/DCAKeep3rJob.sol:DCAKeep3rJob');
-    DCAKeep3rJob = await DCAKeep3rJobFactory.deploy(DCAHubCompanion.address, governor.address);
+    keep3r = await smock.fake('IKeep3rJobs');
+    DCAKeep3rJobFactory = await ethers.getContractFactory('contracts/mocks/DCAKeep3rJob/DCAKeep3rJob.sol:DCAKeep3rJobMock');
+    DCAKeep3rJob = await DCAKeep3rJobFactory.deploy(DCAHubCompanion.address, keep3r.address, governor.address);
+    chainId = BigNumber.from((await ethers.provider.getNetwork()).chainId);
     snapshotId = await snapshot.take();
   });
 
   beforeEach('Deploy and configure', async () => {
     await snapshot.revert(snapshotId);
+    keep3r.isKeeper.reset();
+    keep3r.worked.reset();
   });
 
   describe('constructor', () => {
@@ -32,7 +42,16 @@ contract('DCAKeep3rJob', () => {
       then('deployment is reverted with reason', async () => {
         await behaviours.deployShouldRevertWithMessage({
           contract: DCAKeep3rJobFactory,
-          args: [constants.ZERO_ADDRESS, governor.address],
+          args: [constants.ZERO_ADDRESS, keep3r.address, governor.address],
+          message: 'ZeroAddress',
+        });
+      });
+    });
+    when('keep3r is zero address', () => {
+      then('deployment is reverted with reason', async () => {
+        await behaviours.deployShouldRevertWithMessage({
+          contract: DCAKeep3rJobFactory,
+          args: [DCAHubCompanion.address, constants.ZERO_ADDRESS, governor.address],
           message: 'ZeroAddress',
         });
       });
@@ -41,8 +60,14 @@ contract('DCAKeep3rJob', () => {
       then('companion is set correctly', async () => {
         expect(await DCAKeep3rJob.companion()).to.equal(DCAHubCompanion.address);
       });
+      then('keep3r is set correctly', async () => {
+        expect(await DCAKeep3rJob.keep3r()).to.equal(keep3r.address);
+      });
       then('no address can sign work', async () => {
         expect(await DCAKeep3rJob.canAddressSignWork(signer.address)).to.be.false;
+      });
+      then('nonce starts at 0', async () => {
+        expect(await DCAKeep3rJob.nonce()).to.equal(0);
       });
     });
   });
@@ -119,5 +144,96 @@ contract('DCAKeep3rJob', () => {
       params: () => [constants.NOT_ZERO_ADDRESS],
       governor: () => governor,
     });
+  });
+  describe('work', () => {
+    given(async () => {
+      await DCAKeep3rJob.connect(governor).setIfAddressCanSign(signer.address, true);
+    });
+
+    workFailsTest({
+      when: 'caller is not a keep3r',
+      signer: () => signer,
+      callerIsNotAKeeper: true,
+      txFailsWith: 'NotAKeeper',
+    });
+    workFailsTest({
+      when: 'signer is not allowed to sign',
+      signer: () => random,
+      txFailsWith: 'SignerCannotSignWork',
+    });
+    workFailsTest({
+      when: 'nonce is invalid',
+      signer: () => signer,
+      nonce: 10,
+      txFailsWith: 'InvalidNonce',
+    });
+    workFailsTest({
+      when: 'deadline has expired',
+      signer: () => signer,
+      deadline: moment().unix() - 200000,
+      txFailsWith: 'DeadlineExpired',
+    });
+    workFailsTest({
+      when: 'chain id is invalid',
+      signer: () => signer,
+      chainId: 69,
+      txFailsWith: 'InvalidChainId',
+    });
+
+    when('work is called correctly', () => {
+      const CALL: WorkCall = { call: utils.hexlify(utils.randomBytes(10)), nonce: 0, chainId, deadline: constants.MAX_UINT_256 };
+      let caller: Wallet;
+      given(async () => {
+        caller = await wallet.generateRandom();
+        const { bytes, signature } = await sign(signer, CALL);
+        keep3r.isKeeper.returns(true);
+        await DCAKeep3rJob.connect(caller).work(bytes, signature);
+      });
+      then('nonce is increased', async () => {
+        expect(await DCAKeep3rJob.nonce()).to.equal(1);
+      });
+      then('companion is called correctly', async () => {
+        expect(await DCAKeep3rJob.companionCalledWith()).to.equal(CALL.call);
+      });
+      then('worked is called', () => {
+        expect(keep3r.worked).to.have.been.calledOnceWith(caller.address);
+      });
+    });
+
+    function workFailsTest({
+      when: title,
+      signer,
+      txFailsWith,
+      callerIsNotAKeeper,
+      ...call
+    }: { when: string; signer: () => SignerWithAddress; callerIsNotAKeeper?: boolean; txFailsWith: string } & Partial<WorkCall>) {
+      when(title, () => {
+        given(() => keep3r.isKeeper.returns(!callerIsNotAKeeper));
+        then('reverts with message', async () => {
+          const { bytes, signature } = await sign(signer(), call);
+          await behaviours.txShouldRevertWithMessage({
+            contract: DCAKeep3rJob,
+            func: 'work',
+            args: [bytes, signature],
+            message: txFailsWith,
+          });
+        });
+      });
+    }
+
+    async function sign(signer: SignerWithAddress, call: Partial<WorkCall>) {
+      const bytes = encode(call);
+      const messageHash = ethers.utils.solidityKeccak256(['bytes'], [bytes]);
+      const signature = await signer.signMessage(ethers.utils.arrayify(messageHash));
+      return { bytes, signature };
+    }
+    function encode({ call, nonce, chainId: sentChainId, deadline }: Partial<WorkCall>) {
+      const coder = new ethers.utils.AbiCoder();
+      return coder.encode(
+        ['tuple(bytes, uint256, uint256, uint256)'],
+        [[call ?? '0x', nonce ?? 0, sentChainId ?? chainId, deadline ?? constants.MAX_UINT_256]]
+      );
+    }
+    type WorkCall = { call: BytesLike; nonce: number; chainId: BigNumberish; deadline: BigNumberish };
   });
 });
