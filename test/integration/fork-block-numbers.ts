c4e2495fdc8bf169f482d32c333df336ae513d9c
Ged <0xged@protonmail.com>
2022-04-24 22:43:12 -0300

    fix: beta migrator integration test

--- test/integration/fork-block-numbers.ts
+++ test/integration/fork-block-numbers.ts
@@ -2,7 +2,7 @@ const forkBlockNumber = {
   'swap-for-caller': 14650320, // Ethereum
   wtoken: 14650320, // Ethereum
   multicall: 14650320, // Ethereum
-  'beta-migrator': 3856700, // Optimism
+  'beta-migrator': 3029394, // Optimism
   'keep3r-job': 14585044, // Ethereum
 };
 
