de38ee7394c031c29208fbcc9401f12492811ce7
Sam Bugs <sambugs@protonmail.com>
2022-08-02 00:18:57 -0300

    fix: avoid abstract

--- contracts/DCAHubSwapper/DCAHubSwapper.sol
+++ contracts/DCAHubSwapper/DCAHubSwapper.sol
@@ -7,7 +7,7 @@ import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '../interfaces/IDCAHubSwapper.sol';
 import './utils/DeadlineValidation.sol';
 
-abstract contract DCAHubSwapper is DeadlineValidation, AccessControl, SwapAdapter, IDCAHubSwapper {
+contract DCAHubSwapper is DeadlineValidation, AccessControl, SwapAdapter, IDCAHubSwapper {
   enum SwapPlan {
     // Used only for tests
     NONE,
