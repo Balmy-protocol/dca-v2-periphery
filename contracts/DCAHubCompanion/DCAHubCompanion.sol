09387ec123cea6fb72db0109d2c992bd92e95669
Sam Bugs <sambugs@protonmail.com>
2022-08-02 11:25:24 -0300

    refactor: create base companion

--- contracts/DCAHubCompanion/DCAHubCompanion.sol
+++ contracts/DCAHubCompanion/DCAHubCompanion.sol
@@ -1,21 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.8.7 <0.9.0;
 
-import '@mean-finance/swappers/solidity/contracts/extensions/GetBalances.sol';
-import '@mean-finance/swappers/solidity/contracts/extensions/RevokableWithGovernor.sol';
 import './DCAHubCompanionLibrariesHandler.sol';
 import './DCAHubCompanionHubProxyHandler.sol';
-import './DCAHubCompanionTakeSendAndSwapHandler.sol';
-import './utils/Multicall.sol';
+import '../utils/BaseCompanion.sol';
 
-contract DCAHubCompanion is
-  DCAHubCompanionLibrariesHandler,
-  DCAHubCompanionHubProxyHandler,
-  DCAHubCompanionTakeSendAndSwapHandler,
-  RevokableWithGovernor,
-  GetBalances,
-  Multicall,
-  IDCAHubCompanion
-{
-  constructor(address _swapperRegistry, address _governor) SwapAdapter(_swapperRegistry) Governable(_governor) {}
+contract DCAHubCompanion is DCAHubCompanionLibrariesHandler, DCAHubCompanionHubProxyHandler, BaseCompanion, IDCAHubCompanion {
+  constructor(address _swapperRegistry, address _governor) BaseCompanion(_swapperRegistry, _governor) {}
 }
