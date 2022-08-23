29280a539f46588454a1c17669c44dcc0cce7688
Ged <0xged@protonmail.com>
2022-08-23 13:48:07 -0300

    fix: only skip gas limit on coverage

--- deploy/001_companion.ts
+++ deploy/001_companion.ts
@@ -19,7 +19,7 @@ const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnviro
       values: [swapperRegistry.address, msig],
     },
     log: !process.env.TEST,
-    overrides: !!process.env.TEST
+    overrides: !!process.env.COVERAGE
       ? {}
       : {
           gasLimit: 6_000_000,
