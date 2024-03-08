fbee58a6d1defbf0c82c58f70a4be0e5677f34c4
Sam Bugs <101145325+0xsambugs@users.noreply.github.com>
2024-03-08 10:09:54 -0300

    refactor: simplify fee manager (#207)

--- contracts/interfaces/IDCAFeeManager.sol
+++ contracts/interfaces/IDCAFeeManager.sol
@@ -34,60 +34,12 @@ interface IDCAFeeManager {
     address allowanceTarget;
   }
 
-  /// @notice Represents a share of a target token
-  struct TargetTokenShare {
-    address token;
-    uint16 shares;
-  }
-
-  /// @notice Represents how much to deposit to a position, for a specific token
-  struct AmountToFill {
-    address token;
-    uint32 amountOfSwaps;
-    uint256 amount;
-  }
-
   /// @notice Represents how much is available for withdraw, for a specific token
   struct AvailableBalance {
     address token;
     uint256 platformBalance;
     uint256 feeManagerBalance;
-    PositionBalance[] positions;
   }
-
-  /// @notice Represents information about a specific position
-  struct PositionBalance {
-    uint256 positionId;
-    IERC20Metadata from;
-    IERC20Metadata to;
-    uint256 swapped;
-    uint256 remaining;
-  }
-
-  /**
-   * @notice The contract's owner and other allowed users can specify the target tokens that the fees
-   *         should be converted to. They can also specify the percentage assigned to each token
-   * @dev This value is constant and cannot be modified
-   * @return The numeric value that represents a 100% asignment for the fee conversion distribution
-   */
-  // solhint-disable-next-line func-name-mixedcase
-  function MAX_TOKEN_TOTAL_SHARE() external view returns (uint16);
-
-  /**
-   * @notice Returns the swap interval used for DCA swaps
-   * @dev This value is constant and cannot be modified
-   * @return The swap interval used for DCA swaps
-   */
-  // solhint-disable-next-line func-name-mixedcase
-  function SWAP_INTERVAL() external view returns (uint32);
-
-  /**
-   * @notice Returns the position id for a given (from, to) pair
-   * @dev Key for (tokenA, tokenB) is different from the key for(tokenB, tokenA)
-   * @param pairKey The key of the pair (from, to)
-   * @return The position id for the given pair
-   */
-  function positions(bytes32 pairKey) external view returns (uint256); // key(from, to) => position id
 
   /**
    * @notice Executes multiple swaps
@@ -116,48 +68,6 @@ interface IDCAFeeManager {
    * @param recipient The address of the recipient
    */
   function withdrawFromBalance(IDCAHub.AmountOfToken[] calldata amountToWithdraw, address recipient) external;
-
-  /**
-   * @notice Withdraws tokens from the given positions, and sends them to the given recipient
-   * @dev Can only be executed by admins
-   * @param hub The address of the DCA Hub
-   * @param positionSets The positions to withdraw from
-   * @param recipient The address of the recipient
-   */
-  function withdrawFromPositions(
-    IDCAHub hub,
-    IDCAHub.PositionSet[] calldata positionSets,
-    address recipient
-  ) external;
-
-  /**
-   * @notice Takes a certain amount of the given tokens, and sets up DCA swaps for each of them.
-   *         The given amounts can be distributed across different target tokens
-   * @dev Can only be executed by admins
-   * @param hub The address of the DCA Hub
-   * @param amounts Specific tokens and amounts to take from this contract and send to the hub
-   * @param distribution How to distribute the source tokens across different target tokens
-   */
-  function fillPositions(
-    IDCAHub hub,
-    AmountToFill[] calldata amounts,
-    TargetTokenShare[] calldata distribution
-  ) external;
-
-  /**
-   * @notice Takes list of position ids and terminates them. All swapped and unswapped balance is
-   *         sent to the given recipient. This is meant to be used only if for some reason swaps are
-   *         no longer executed
-   * @dev Can only be executed by admins
-   * @param hub The address of the DCA Hub
-   * @param positionIds The positions to terminate
-   * @param recipient The address that will receive all swapped and unswapped tokens
-   */
-  function terminatePositions(
-    IDCAHub hub,
-    uint256[] calldata positionIds,
-    address recipient
-  ) external;
 
   /**
    * @notice Revokes ERC20 allowances for the given spenders
--- contracts/mocks/DCAFeeManager/DCAFeeManager.sol
+++ contracts/mocks/DCAFeeManager/DCAFeeManager.sol
@@ -33,24 +33,6 @@ contract DCAFeeManagerMock is DCAFeeManager {
     return _revokeCalls;
   }
 
-  function setPosition(
-    address _from,
-    address _to,
-    uint256 _positionId
-  ) external {
-    positions[getPositionKey(_from, _to)] = _positionId;
-  }
-
-  function positionsWithToken(address _toToken) external view returns (uint256[] memory) {
-    return _positionsWithToken[_toToken];
-  }
-
-  function setPositionsWithToken(address _toToken, uint256[] calldata _positionIds) external {
-    for (uint256 i; i < _positionIds.length; i++) {
-      _positionsWithToken[_toToken].push(_positionIds[i]);
-    }
-  }
-
   function _sendBalanceOnContractToRecipient(address _token, address _recipient) internal override {
     _sendBalanceOnContractToRecipientCalls.push(SendBalanceOnContractToRecipientCall(_token, _recipient));
   }
